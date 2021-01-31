9eb856402b8958e0fcbbeebd3620fcf78321ccbc
Yusufbbli34@outlook.com <Yusufbbli34@outlook.com>
2021-01-31 21:15:39 +0300

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+            cities.Add("adana");
+                                                            
+            MyList<string> cities2 = new MyList<string>();
+            cities2.Add("samsun");
+        }
+
+    }
+
+    class MyList<T>
+    {
+        T[] array;
+        public void Add(T item)
+        {
+
+        }
+    }
+}
